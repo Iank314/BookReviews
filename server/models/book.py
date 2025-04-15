e8109a1c9c92c16475816df6c85f21173b1332e1
Ian Kaufman <Kaufmanian49@gmail.com>
2025-04-15 19:38:36 -0400

    AllClassesAdded

---
+++ server/models/book.py
@@ -0,0 +1,8 @@
+class Books:
+    def __init__(self, id, title, authors, description, tags, metadata):
+        self.id = id
+        self.title = title
+        self.authors = authors
+        self.description = description
+        self.tags = tags
+        self.metadata = metadata 
